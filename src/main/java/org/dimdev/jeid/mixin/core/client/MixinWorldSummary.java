2ee4547c31955a3ec50ce20f497a2ab879ac32a6
Runemoro <runemoro@live.ca>
2018-07-15 18:28:44 -0400

    Fix JEID version check

--- src/main/java/org/dimdev/jeid/mixin/core/client/MixinWorldSummary.java
+++ src/main/java/org/dimdev/jeid/mixin/core/client/MixinWorldSummary.java
@@ -3,12 +3,14 @@ package org.dimdev.jeid.mixin.core.client;
 import net.minecraft.world.storage.WorldSummary;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(WorldSummary.class)
 public class MixinWorldSummary {
     @Shadow @Final private int versionId;
 
+    @Overwrite
     public boolean askToOpenWorld() {
         return versionId > 1343 && versionId != Integer.MAX_VALUE / 2;
     }
