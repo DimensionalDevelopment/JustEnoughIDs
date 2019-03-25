8d2e237a3ae49a7751facf195616b47f456fb60c
Sam Kirby <sam.kirby94@hotmail.co.uk>
2019-03-25 14:20:37 +0000

    Update Abyssalcraft mixin to adapt new BiomeUtil class No longer need mixin for CleansingRitualMessage Addresses DarkPacks/SevTech-Ages#3639

---
+++ src/main/java/org/dimdev/jeid/mixin/modsupport/MixinBiomeUtil.java
@@ -0,0 +1,26 @@
+package org.dimdev.jeid.mixin.modsupport;
+
+import com.shinoow.abyssalcraft.common.network.PacketDispatcher;
+import com.shinoow.abyssalcraft.common.network.client.CleansingRitualMessage;
+import com.shinoow.abyssalcraft.common.util.BiomeUtil;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import org.dimdev.jeid.INewChunk;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Pseudo;
+
+@Pseudo
+@Mixin(BiomeUtil.class)
+public class MixinBiomeUtil {
+    @Overwrite(remap = false)
+    public static void updateBiome(World worldIn, BlockPos pos, int b, boolean batched) {
+        Chunk c = worldIn.getChunk(pos);
+        ((INewChunk) c).getIntBiomeArray()[(pos.getZ() & 0xF) << 4 | pos.getX() & 0xF] = b;
+        c.setModified(true);
+        if(!worldIn.isRemote) {
+            PacketDispatcher.sendToDimension(new CleansingRitualMessage(pos.getX(), pos.getZ(), b, batched), worldIn.provider.getDimension());
+        }
+    }
+}
