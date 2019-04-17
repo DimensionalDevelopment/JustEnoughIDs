58ec790415c78656132ee651b8d3fbf40055b575
ZombieHDGaming <eiondailey@live.com>
2019-04-17 17:39:33 -0400

    Creeping Nether support fixed (?) This will definitely need testing, but it *should* work in theory

--- src/main/java/org/dimdev/jeid/mixin/modsupport/creepingnether/MixinCorruptorAbstract.java
+++ src/main/java/org/dimdev/jeid/mixin/modsupport/creepingnether/MixinCorruptorAbstract.java
@@ -1,6 +1,6 @@
 package org.dimdev.jeid.mixin.modsupport.creepingnether;
 
-import com.cutievirus.creepingnether.entity.EntityPortal;
+import com.cutievirus.creepingnether.entity.CorruptorAbstract;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
@@ -12,10 +12,13 @@ import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Pseudo
-@Mixin(EntityPortal.class)
-public class MixinEntityPortal {
+@Mixin(CorruptorAbstract.class)
+public class MixinCorruptorAbstract {
     @Shadow private static Biome toBiome;
 
+    /**
+     * This should(?) fix Creeping Nether issues
+     */
     @Overwrite(remap = false)
     public static void corruptBiome(World world, BlockPos pos) {
         if (world.isBlockLoaded(pos)) {
