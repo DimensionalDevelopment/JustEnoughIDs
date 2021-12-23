78c5cba72740cbe817db1f02efdd97d141420f41
Mysticpasta1 <ethan.cantrell@asub.edu>
2021-12-23 01:03:40 -0600

    possibly fix mixin bug

--- src/main/java/org/dimdev/jeid/mixin/init/JEIDMixinLoader.java
+++ src/main/java/org/dimdev/jeid/mixin/init/JEIDMixinLoader.java
@@ -1,25 +1,17 @@
 package org.dimdev.jeid.mixin.init;
 
-import net.minecraft.launchwrapper.Launch;
-import net.minecraftforge.fml.common.Loader;
-import net.minecraftforge.fml.common.ModClassLoader;
-import net.minecraftforge.fml.common.ModContainer;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.transformer.MixinProcessor;
-import org.spongepowered.asm.mixin.transformer.Proxy;
+import zone.rong.mixinbooter.MixinLoader;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.util.List;
+@MixinLoader
+public class JEIDMixinLoader {
+    {
+        Mixins.addConfiguration("mixins.jeid.modsupport.json");
+        Mixins.addConfiguration("mixins.jeid.twilightforest.json");
+    }
+}
 
-@Mixin(Loader.class)
+/*@Mixin(Loader.class)
 public class MixinLoader {
     @Shadow private List<ModContainer> mods;
     @Shadow private ModClassLoader modClassLoader;
@@ -27,7 +19,7 @@ public class MixinLoader {
     /**
      * @reason Load all mods now and load mod support mixin configs. This can't be done later
      * since constructing mods loads classes from them.
-     */
+     /
     @Inject(method = "loadMods", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fml/common/LoadController;transition(Lnet/minecraftforge/fml/common/LoaderState;Z)V", ordinal = 1), remap = false)
     private void beforeConstructingMods(List<String> injectedModContainers, CallbackInfo ci) {
         // Add all mods to class loader
@@ -68,4 +60,4 @@ public class MixinLoader {
             throw new RuntimeException(e);
         }
     }
-}
+}*/
