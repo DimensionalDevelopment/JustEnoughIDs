c82ad3e2f7041cf348b0d5a63a6c096e2d0a7ac7
Mysticpasta1 <ethan.cantrell@asub.edu>
2021-12-24 13:42:08 -0600

    push

--- src/main/java/org/dimdev/jeid/mixin/init/JEIDMixinLoader.java
+++ src/main/java/org/dimdev/jeid/mixin/init/JEIDMixinLoader.java
@@ -1,27 +1,36 @@
 package org.dimdev.jeid.mixin.init;
 
+import net.minecraft.launchwrapper.Launch;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.ModClassLoader;
+import net.minecraftforge.fml.common.ModContainer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
-import zone.rong.mixinbooter.MixinLoader;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.transformer.MixinProcessor;
 
-@MixinLoader
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.MalformedURLException;
+import java.util.List;
+
+@Mixin(Loader.class)
 public class JEIDMixinLoader {
-    {
-        Mixins.addConfiguration("mixins.jeid.modsupport.json");
-        Mixins.addConfiguration("mixins.jeid.twilightforest.json");
-    }
-}
-
-/*@Mixin(Loader.class)
-public class MixinLoader {
-    @Shadow private List<ModContainer> mods;
+    @Shadow
+    private List<ModContainer> mods;
     @Shadow private ModClassLoader modClassLoader;
 
     /**
      * @reason Load all mods now and load mod support mixin configs. This can't be done later
      * since constructing mods loads classes from them.
-     /
+     */
     @Inject(method = "loadMods", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fml/common/LoadController;transition(Lnet/minecraftforge/fml/common/LoaderState;Z)V", ordinal = 1), remap = false)
-    private void beforeConstructingMods(List<String> injectedModContainers, CallbackInfo ci) {
+    private void beforeConstructingMods(List<String> nonMod, CallbackInfo ci) {
         // Add all mods to class loader
         for (ModContainer mod : mods) {
             try {
@@ -60,4 +69,4 @@ public class MixinLoader {
             throw new RuntimeException(e);
         }
     }
-}*/
+}
